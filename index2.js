5e60667ecedc723e0f0d2706a193f4afbbd8e2b7
Nassiwa Veronica <veronicanassiwa@gmail.com>
2020-01-14 10:02:30 -0800

    events practice

---
+++ index2.js
@@ -0,0 +1,14 @@
+
+
+
+function myfunction(){
+   let firstname= document.getElementById("fn").Value;
+   let lastname=document.getElementById("ln").Value;
+   let name= firstname + lastname;
+
+    document.getElementById("vero").innerHTML="Your name is" + " " + name;
+   
+}
+console.log("Hellos");
+console.error("erro");
+
