e7fd844f8ceaa147275b13f523bd9e8037efbbfb
Nassiwa Veronica <veronicanassiwa@gmail.com>
2020-01-15 05:45:36 -0800

    events

--- index2.js
+++ index2.js
@@ -1,14 +1,42 @@
 
 
 
-function myfunction(){
-   let firstname= document.getElementById("fn").Value;
-   let lastname=document.getElementById("ln").Value;
-   let name= firstname + lastname;
-
-    document.getElementById("vero").innerHTML="Your name is" + " " + name;
-   
+function addd(){
+    let fnumber=document.getElementById("fnumberr").value;
+    let lsnumber=document.getElementById("lnumberr").value;
+    let x= parseInt(fnumber) + parseInt(lsnumber);
+    document.getElementById("vero").innerHTML="The sum of the values is" + x;
 }
-console.log("Hellos");
-console.error("erro");
+function multii(){
+let number1=document.getElementById("fnumberr").value;
+let number2=document.getElementById("lnumberr").value;
+let z=number1*number2;
+document.getElementById("vero").innerHTML="The answer is " + z;
+number1.addEventListener("click",function (){
+    alert("hey");
+});
+}
 
+function subb(){
+let no1=document.getElementById("fnumberr").value;
+let no2=document.getElementById("lnumberr").value;
+let y=no1-no2;
+document.getElementById("vero").innerHTML="The difference is " + y;
+
+}
+
+function squareroot(){
+ let y=document.getElementById("fnumbee").value;
+ let Z=document.getElementById("lnumbee").value;
+ let x=Math.sqrt(y,Z);
+ document.getElementById("hus").innerHTML="The sauareroot of the two numbers is" + " " + x;
+
+} 
+
+function roundoff(){
+let v =document.getElementById("fnumbee").value;
+let n=document.getElementById("lnumbee").value;
+let c=Math.round(v,n);
+document.getElementById("hus").innerHTML="The answer is" + " " + c;
+
+}
